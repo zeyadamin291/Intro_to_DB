27d6bb877d0319c382f01ff7413f74841382bd9b
Zeyad Amin <zeyadamin291@gmail.com>
2025-11-26 20:51:29 +0200

    task5

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 USE DATABASE alx_book_store
-INSERT INTO Customers (customer_id , customer_name,email, address) 
+INSERT INTO customer (customer_id , customer_name,email, address) 
 VALUES (1 ,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave')
