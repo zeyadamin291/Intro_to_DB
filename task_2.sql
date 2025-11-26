dc4e6b197081ae5b3bf0446a632242f93baf60ad
Zeyad Amin <zeyadamin291@gmail.com>
2025-11-26 16:23:56 +0200

    task2

---
+++ task_2.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT 
+);
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
