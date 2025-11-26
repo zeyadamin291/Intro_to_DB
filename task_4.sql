aac1818f1ec2752a29b8b54a59966366b9b4d5f9
Zeyad Amin <zeyadamin291@gmail.com>
2025-11-26 20:33:36 +0200

    task4

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,12 @@
 USE alx_book_store;
 
-SELECT * FROM Books;
-SELECT * FROM Authors;
-SELECT * FROM Customers;
-SELECT * FROM Orders;
-SELECT * FROM Order_Details;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = "Books"
